ffbcaf04cc4dd0e8eda597b2369ac4c8beabfe24
Phurky <47676182+Phurky@users.noreply.github.com>
2022-09-18 17:27:49 +0300

    Delete variables.java

--- variables.java
+++
@@ -1,9 +0,0 @@
-public class variables {
-    public static void main(String[] args) {
-        int numberOfStudents = 10;
-        String message = "number of students : ";
-        System.out.println(message + numberOfStudents);
-
-
-    }
-}
